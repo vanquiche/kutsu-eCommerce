ce76d90836fcecf02598f6327da092ac06f0ee28
vanquiche <stevevang2010@hotmail.com>
2022-04-01 11:07:32 +0900

    create context and reducer function

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,23 @@
 import React, { createContext, useReducer } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductPage from './pages/ProductPage';
 import './App.css';
-
-type Action = 'increment' | 'decrement';
-type Cart = { cart: number };
-
-interface CartContextType {
-  state: Cart;
-  dispatch: React.Dispatch<Action>;
-}
-
-const initialState = { cart: 0 };
-
-export const CartContext = createContext<CartContextType>({
-  state: initialState,
-  dispatch: () => {},
-});
-
-const reducer = (state: Cart, action: Action) => {
-  switch (action) {
-    case 'increment':
-      return { cart: state.cart + 1 };
-    case 'decrement':
-      return { cart: state.cart - 1 };
-    default:
-      // throw new Error();
-      return state;
-  }
-};
 
 function App() {
   console.log('render App');
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
-    <CartContext.Provider value={{ state, dispatch }}>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<Home />} />
-        </Route>
-        {/* <Route /> */}
-      </Routes>
-    </CartContext.Provider>
+    <Routes>
+      <Route path='/'>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path='shop'>
+        <Route index element={<Shop />} />
+        <Route path=':id' element={<ProductPage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,54 @@
-import React, {useState, createContext} from 'react'
+import React, { createContext } from 'react';
 
+export interface Product {
+  id: string;
+  item: string;
+  qty: number;
+  price: number;
+  image?: string;
+}
 
+interface CartContextType {
+  state: Product[];
+  dispatch: React.Dispatch<Action>;
+}
 
-export const CartContext = createContext(null)
+export type Action = {
+  type: 'increment' | 'decrement' | 'add item' | 'remove item';
+  product?: Product;
+};
+
+export const initialState = [];
+
+export const CartContext = createContext<CartContextType>({
+  state: initialState,
+  dispatch: () => {},
+});
+
+export const reducer = (state: Product[], action: Action): Product[] => {
+  switch (action.type) {
+    case 'increment':
+      return state.map((product) => {
+        if (product.id === action.product!.id) {
+          return { ...product, qty: product.qty + 1 };
+        } else return product;
+      });
+    case 'decrement':
+      return state.map((product) => {
+        if (product.id === action.product!.id) {
+          return { ...product, qty: product.qty - 1 };
+        } else return product;
+      });
+    case 'add item':
+      return [...state, Object.assign({}, action.product)]
+    case 'remove item':
+      return state.filter((product) => {
+        if (product.id !== action.product!.id) {
+          return product;
+        }
+      });
+    default:
+      // throw new Error();
+      return state;
+  }
+};
