59405254b69486d221ab3ab24045375836fe3c02
vanquiche <stevevang2010@hotmail.com>
2022-04-01 13:07:30 +0900

    create dumb product component

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,38 +1,25 @@
-import React, { useContext } from 'react';
-import { CartContext } from '../../contexts/CartContext';
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Product } from '../../contexts/CartContext';
+import './Product.css';
 
-const Product = () => {
-  const { dispatch } = useContext(CartContext);
+interface Props {
+  product?: Product;
+}
+
+const Product: React.FC<Props> = ({ product }) => {
   console.log('render Product');
 
-  const handleClick = () => {
-    const newProduct = {
-      id: Math.random().toString(),
-      item: 'Milk',
-      qty: 1,
-      price: 5.99
-    }
-    dispatch({type: 'add item', product: newProduct})
-  }
-
   return (
-    <div style={{ margin: '1rem 2rem' }}>
-      <h1>Products</h1>
-      {/* <p>{state.cart}</p> */}
-      {/* <button
-          style={{ display: 'block', margin: '1rem 0' }}
-          onClick={() => dispatch({type: 'increment'})}
-        >
-          add
-        </button>
-        <button
-          style={{ display: 'block', margin: '1rem 0' }}
-          onClick={() => dispatch({type: 'decrement'})}
-        >
-          subtract
-        </button> */}
-
-        <button onClick={handleClick}>add Product</button>
+    <div className='product-container'>
+      <LazyLoadImage
+        alt='placeholder image'
+        src='https://via.placeholder.com/200'
+        className='product-image'
+        effect='blur'
+      />
+      <p className='product-name'>{product?.item}</p>
+      <p className='product-price'>{product?.price}</p>
     </div>
   );
 };
