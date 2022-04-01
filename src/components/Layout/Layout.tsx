ce1951bf02f3cbf463575105c4d14f1ed658e545
vanquiche <stevevang2010@hotmail.com>
2022-04-01 11:15:15 +0900

    add context provider to header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import {
+  CartContext,
+  reducer,
+  initialState,
+} from '../../contexts/CartContext';
 
-const Layout = (props: {children: React.ReactNode}) => {
+const Layout = (props: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <>
+    <CartContext.Provider value={{ state, dispatch }}>
       <Header />
       {props.children}
+
       <Footer />
-    </>
+    </CartContext.Provider>
   );
 };
 
