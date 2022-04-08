7b56f46bf512f84d9d236211a38fe36baa252ae5
vanquiche <stevevang2010@hotmail.com>
2022-04-09 01:00:48 +0900

    fix link to path

--- src/components/Products/Recommended.tsx
+++ src/components/Products/Recommended.tsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import { list } from '../../db/server';
+import { v4 } from 'uuid';
 import './Product.css';
 import ProductThumb from './ProductThumb';
-import { useLocation, matchPath, Link } from 'react-router-dom';
 
 const sliceList = () => {
   let newList = [];
-
   for (let i = 0; i < 4; i++) {
     const random = Math.floor(Math.random() * list.length);
     newList.push(list[random]);
   }
-
   return newList;
 };
 
-const recommendList = sliceList();
-
 const Recommended = () => {
-  // const {pathname} = matchPath
-  const {pathname} = useLocation();
-  const shopPath = matchPath('shop/', pathname)
-  console.log(shopPath)
+  const recommendList = sliceList();
+
   return (
     <div className='recommended-container'>
       <p className='recommended-title'>RECOMMENDATIONS FOR YOU</p>
       <div className='recommended-thumbs'>
         {recommendList.map((product) => (
-          <ProductThumb product={product} />
-        // <Link to={product.id}>{product.item}</Link>
+          <ProductThumb
+            product={product}
+            keyRef={v4()}
+            segment={`/shop/${product.id}`}
+            handleClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
+          />
         ))}
       </div>
-
     </div>
   );
 };
