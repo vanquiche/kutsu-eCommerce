24cd23ece5b03c8c2e1197731f0d62f673fee06c
vanquiche <stevevang2010@hotmail.com>
2022-04-19 19:33:36 +0900

    install react-slick library
    - install and add carousel css to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
